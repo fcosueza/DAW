cbca8be38369bb11f380519b09b066b945927eaa
Fco Sueza <fcosueza@disroot.org>
2023-05-17 13:30:53 +0200

    Added solution to ED\'s exercises Part I

---
+++ ED/Tareas/Tema 5/ingenieriaInversa_FJS2223/src/figuras/CuadradoFJS2223.java
@@ -0,0 +1,13 @@
+package figuras;
+
+/**
+ *
+ * @author Ana Espina Martínez
+ */
+public class CuadradoFJS2223 extends Rectangulo {
+
+    public CuadradoFJS2223(double lado) {
+        super(lado, lado);
+    }
+    
+}
---
+++ ED/Tareas/Tema 5/ingenieriaInversa_FJS2223/src/figuras/Main.java
@@ -0,0 +1,18 @@
+package figuras;
+
+/**
+ *
+ * @author Ana Espina Martínez
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        CuadradoFJS2223 cuadrado = new CuadradoFJS2223(5);
+        
+        System.out.println("Area: " + cuadrado.getArea());
+        System.out.println("Perímetro: " + cuadrado.getPerimetro());
+        
+    }
+    
+}
