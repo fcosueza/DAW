7529450c1b62a072d4260f93176c7856218d3d63
Fco Sueza <fcosueza@disroot.org>
2023-11-06 10:37:46 +0100

    Added folder ejecicios in PROG. It will holds exercises of every subject

---
+++ PROG/Ejercicios/Tema1/src/main/java/com/daw/tema1/NumeroPositivo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.daw.tema1;
+
+import java.util.Scanner;
+
+/**
+ * Ejercicio para determinar si un número introducido por teclado es positivo o
+ * negativo.
+ * 
+ * @author fcosueza
+ */
+public class NumeroPositivo {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+        
+        System.out.println("Introduzca un ńumero entero:");
+        numero = teclado.nextInt();
+        
+        if (numero >= 0) 
+            System.out.println("El número es positivo.");
+        else 
+            System.out.println("El número es negativo.");
+    }
+}
