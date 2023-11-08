cbc6b4f16ecb4ecd38fa6301cf35edfe439a5e8f
Fco Sueza <fcosueza@disroot.org>
2023-11-08 08:25:54 +0100

    Modified exercise in PROG

--- PROG/Ejercicios/Tema1/src/main/java/daw/ejercicios/tema1/Aprobado.java
+++ PROG/Ejercicios/Tema1/src/main/java/daw/ejercicios/tema1/Aprobado.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.daw.tema1;
+package daw.ejercicios.tema1;
 
 import java.util.Scanner;
 
--- PROG/Ejercicios/Tema1/src/main/java/daw/ejercicios/tema1/NumeroPositivo.java
+++ PROG/Ejercicios/Tema1/src/main/java/daw/ejercicios/tema1/NumeroPositivo.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.daw.tema1;
+package daw.ejercicios.tema1;
 
 import java.util.Scanner;
 
