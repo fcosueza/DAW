103007ff1c787127c1cacf599c3542c5ead6ab2f
Fco Sueza <fcosueza@disroot.org>
2023-11-10 10:39:56 +0100

    Added java code to resolve line and square formation in PROG\'s exercise 2.2

--- PROG/Tareas/Tema 2/PROG02_Tarea_2023_24_Alumnado/PROG02_Tarea_2023_24_Alumnado/src/tarea02/Ejercicio03.java
+++ PROG/Tareas/Tema 2/PROG02_Tarea_2023_24_Alumnado/PROG02_Tarea_2023_24_Alumnado/src/tarea02/Ejercicio03.java
@@ -3,39 +3,94 @@ package tarea02;
 import java.util.Scanner;
 
 /**
- *
- * @author luisnavarro
+ * Clase que pide un número de soldados romanos por teclado, representandolos por 
+ * un asterisco (*) y los representa según la formación elegida por el usuario, pudiendo
+ * ser esta en Linea, Cuadrado o Triangulo.
+ *  
+ * @author Francisco Javier Sueza Rodríguez
  */
 public class Ejercicio03 {
-
+    
     public static void main(String args[]) {
-        //----------------------------------------------
+        //---------------------------------------------------------------
         //               Declaración de variables y constantes
-        //----------------------------------------------
+        //---------------------------------------------------------------
+        
+        // Constantes
+        
+        /*
+         * Vamos a declarar como constantes los carácteres que representan tanto a los 
+         * soldados como los espacios, asi si queremos representarlos usando otros
+         * solo tendremos que cambiar las constantes.
+         */
+        
+        final char SOLDADO_CHAR = '*';
+        final char ESPACIO_CHAR = ' ';
+        
+        // Variables de Entrada
+        
+        int soldados;
+        String formacion;
+        
+        // Variables de Salida
         
         String resultado="";
+        
+        // Variables Auxiliares
+        
+        boolean numeroCorrecto = false;
+        int soldadosEnFormacion = 0;
+        
+        // Variable para la entrada por teclado
+        
+        Scanner teclado = new Scanner(System.in);
 
         //----------------------------------------------
         //               Entrada de datos 
         //----------------------------------------------
+        
         System.out.println("Ejercicio03: Formación romana a partir de un número de soldados.");
         System.out.println("----------------------------------------------------");
-        // Bloque 1. Solicitud del número de soldados
-        // Validación de entrada: Deberíamos comprobar que es mayor o igual que uno. En caso contrario volvemos solicitar el número de soldados
-        // Se supone que nos introducen un número entero. En caso que no sea así saltará una excepción, cuyo tratamiento veremos en uinidades posteriores
+       
+        do {
+            
+            System.out.println("");
+            System.out.println("Introduzca el número de soldados (debe ser mayor a 1): ");
+            soldados = teclado.nextInt();
+            
+            if (soldados > 1)
+                numeroCorrecto = true;
+                
+        } while (!numeroCorrecto);
 
-        // 
-        // Bloque 2. Solicitud del tipo de formación. 
-        // Indicaremos que tiene que ser LINEA, CUADRADO o TRIANGULO
-        // Leemos una cadena, puede que no sea una de las anteriores.
+        System.out.println("");
+        System.out.println("Introduzca el tipo de formación (LINEA, CUADRADO o TRIANGULO): ");
+        formacion = teclado.nextLine().toUpperCase();       
         
         //----------------------------------------------
         //                 Procesamiento 
         //----------------------------------------------
-        // Creamos una cadena de texto para ir almacenando el resultado, que sacaremos por pantalla al final
-        // Sólo si la entrada ha sido válida simulamos la formación representando cada soldado con un *
-        // No olvides que Si al hacer la mayor formación posible del tipo indicado con los soldados introducidos sobran soldados, se debe indicar cuantos."
-        // Si el tipo de formación que nos han indicado y que hemos leído como String no coincide con una de las contempladas, indicamos este error almacenando el mensaje oportuno en el String resultado
+        
+        if (!formacion.equals("LINEA") && !formacion.equals("CUADRADO") && !formacion.equals("TRIANGULO")) 
+            resultado = "Opción NO CORRECTA";
+        else {
+            
+            if (formacion.equals("LINEA")) {
+                for (int i = 0; i < soldados; i++)
+                    resultado += SOLDADO_CHAR + ESPACIO_CHAR;
+            
+            } else if (formacion.equals("CUADRADO")) {
+                soldadosEnFormacion = (int) Math.sqrt(soldados);
+                
+                for (int i = 0; i < soldadosEnFormacion; i++) {
+                    for (int j = 0; j < soldadosEnFormacion; j++)
+                        resultado += SOLDADO_CHAR + ESPACIO_CHAR; 
+                    
+                    resultado += '\n';
+                }              
+            }
+            
+        }
 
         //----------------------------------------------
         //              Salida de resultados 
