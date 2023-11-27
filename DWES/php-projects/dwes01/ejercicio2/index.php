7050d8c24376b5887fecebd348119e607e465077
Fco Sueza <fcosueza@disroot.org>
2023-11-27 10:35:47 +0100

    Added project for exercise 2 in DWES - Tema 1

---
+++ DWES/php-projects/dwes01/ejercicio2/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Francisco Javier Sueza Rodríguez" />
+        <meta name="description" content="Web de la Asociación Respira" />
+
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+
+        <title>Asociación Respira</title>
+    </head>
+    <body>
+        <header>
+            <h1>Header</h1>
+        </header>
+        <main>
+            <h1>Content</h1>
+        </main>
+        <footer>
+            <h1>Footer</h1>
+        </footer>
+    </body>
+</html>
