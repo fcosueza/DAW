61d60a3494ef27ec13a2c0b01ef7d0f8f7bd84aa
Fco Sueza <fcosueza@disroot.org>
2023-12-11 09:31:48 +0100

    Added solution to exercise 1 in DWEC - Tema 2

--- DWEC/Tareas/Tema2/js/a.js
+++ DWEC/Tareas/Tema2/js/a.js
@@ -1,3 +1,35 @@
 /*
  * Ejercicio 1
  */
+
+function bruja() {
+  let pregunta = "";
+  let respuesta = "";
+  let count = 1;
+
+  let salir = false;
+  let rand;
+
+  /*
+   * Realizamos la pregunta hasta que se introduzca la cadena DWEC
+   */
+
+  do {
+    pregunta = window.prompt("Introduzca una pregunta (incluya DWEC para acabar: ");
+    rand = Math.random();
+
+    if (pregunta.includes("DWEC")) {
+      respuesta = "A esas preguntas no respondo. Adios";
+      salir = true;
+    } else {
+      respuesta = rand > 0.49 ? "Si" : "No";
+    }
+
+    /*
+     * Una vez procesada la pregunta, se muestra la pregunta y la respuesta.
+     */
+
+    document.write('<p><span style="font-weight: bold;">Pregunta:</span> ¿' + pregunta + "?</p>");
+    document.write('<p><span style="font-weight: bold;">BrujaIA:</span> ' + respuesta + "</p>");
+  } while (!salir);
+}
