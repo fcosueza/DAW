02bac9ad78fcfa373b78622e33d9bb42402504d2
Fco Sueza <fcosueza@disroot.org>
2024-01-04 10:34:26 +0100

    Formated code in cookies project

--- DWES/php-projects/dwes03/cookies/footer.php
+++ DWES/php-projects/dwes03/cookies/footer.php
@@ -1,20 +1,20 @@
-<?php /* Requiere de la existencia de $archivo (nombre del archivo con el fragmento mostrado)
-         o $longitud (longitud de la cadena de carácteres mostrada),
-         sin no existen, no se muestra nada en el footer. */ ?>
-<footer>
 <?php
-    if (isset($archivo))
-    {
-        echo "Fecha y hora del documento: ";
-        echo date('d/m/Y h:m',filemtime($archivo));
-    }
-    elseif (isset($longitud))
-    {
-        echo "Longitud del documento: $longitud carácteres";
-    }
-
-    //TODO: muestra la lista de secciones visitadas 
+  /* Requiere de la existencia de $archivo (nombre del archivo con el fragmento mostrado)
+   * o $longitud (longitud de la cadena de carácteres mostrada),
+   * sin no existen, no se muestra nada en el footer. */
 ?>
-<BR>
-<BR>
+<footer>
+
+    <?php
+      if (isset($archivo)) {
+          echo "Fecha y hora del documento: ";
+          echo date('d/m/Y h:m', filemtime($archivo));
+      } elseif (isset($longitud)) {
+          echo "Longitud del documento: $longitud carácteres";
+      }
+
+      //TODO: muestra la lista de secciones visitadas
+    ?>
+    <BR>
+    <BR>
 </footer>
--- DWES/php-projects/dwes03/cookies/index.php
+++ DWES/php-projects/dwes03/cookies/index.php
@@ -1,66 +1,53 @@
 <?php
+  require_once 'conf.php';
 
-require_once 'conf.php';
-
-$ver=$_GET['ver']??null;
+  $ver = $_GET['ver'] ?? null;
 
 //Si no se ha especificado $_GET['ver'] hemos que $ver tenga el "link" de la sección por defecto
-if (is_null($ver) || empty($ver))
-{
-    //Buscamos la posición del array $secciones donde está el nombre de sección por defecto
-    $posSeccionDefecto=array_search(SECCION_DEFECTO,array_column($secciones,'nombre'));
-    if ($posSeccionDefecto!==false)
-        $ver=$secciones[$posSeccionDefecto]['link'];
-}
+  if (is_null($ver) || empty($ver)) {
+      //Buscamos la posición del array $secciones donde está el nombre de sección por defecto
+      $posSeccionDefecto = array_search(SECCION_DEFECTO, array_column($secciones, 'nombre'));
+      if ($posSeccionDefecto !== false)
+          $ver = $secciones[$posSeccionDefecto]['link'];
+  }
 //$seccion = sección a mostrar
-if (is_string($ver) && ($pos=array_search($ver,array_column($secciones,'link')))!==false)
-{
-    $seccion=$secciones[$pos];
-}
+  if (is_string($ver) && ($pos = array_search($ver, array_column($secciones, 'link'))) !== false) {
+      $seccion = $secciones[$pos];
+  }
 
-//TODO: 
+//TODO:
 // 1.- obtener las cookies y verificar que no ha sido modificada (calcular hash)
 // 2.- deserializar la cookie con la listad e sitios visitados.
 // 3.- añadir la seccion recien visitada sin duplicados (usa la función time() para el momento de acceso), insertando el primero en la cookie
 // 4.- envia la cookie y el hash
 // 5.- en footer.php, muestra la lista de sitios visitados y la fecha en orden cronológico.
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio 1. Tarea 1. </title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ejercicio 1. Tarea 1. </title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
 <?php require 'header.php'; ?>
-<div>
+        <div>
 
 <?php
-
-
 //Buscamos el texto de "link" almacenado en $ver en las secciones y extraemos el archivo o contenido
-if (isset($seccion))
-{
-    if (isset($seccion['contenido']))
-    {
-        $longitud=strlen($seccion['contenido']);
-        echo $seccion['contenido'];
-    }
-    else if (isset($seccion['archivo']) && file_exists($seccion['archivo']))
-    {
-        readfile($archivo=$seccion['archivo']);
-    }
-} 
-elseif ($ver)
-{
-    echo 'Sección <em>"'.htmlentities($ver).'"</em> no encontrada.';
-}
-
+  if (isset($seccion)) {
+      if (isset($seccion['contenido'])) {
+          $longitud = strlen($seccion['contenido']);
+          echo $seccion['contenido'];
+      } else if (isset($seccion['archivo']) && file_exists($seccion['archivo'])) {
+          readfile($archivo = $seccion['archivo']);
+      }
+  } elseif ($ver) {
+      echo 'Sección <em>"' . htmlentities($ver) . '"</em> no encontrada.';
+  }
 ?>
-</div>
-<?php require 'footer.php'; ?>
-</body>
+        </div>
+            <?php require 'footer.php'; ?>
+    </body>
 </html>
