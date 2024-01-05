442d6485eaa6bde97cd0cbd9734b06fc6a239709
Fco Sueza <fcosueza@disroot.org>
2024-01-05 14:50:02 +0100

    Added basic cookie management in exercise 3.3 in DWES

--- DWES/php-projects/dwes03/cookies/footer.php
+++ DWES/php-projects/dwes03/cookies/footer.php
@@ -3,7 +3,7 @@
    * o $longitud (longitud de la cadena de carácteres mostrada),
    * sin no existen, no se muestra nada en el footer. */
 ?>
-<footer>
+<footer style="text-align: left">
 
     <?php
       if (isset($archivo)) {
@@ -13,7 +13,30 @@
           echo "Longitud del documento: $longitud carácteres";
       }
 
-      //TODO: muestra la lista de secciones visitadas
+      print "<br>";
+
+      /*
+       * Para mostrar las páginas se va a usar $lista_actualizada, ya que si
+       * se usa $_COOKIE no muestra la lista actulizada hasta que se cambia
+       * de enlace, y no se consigue el efecto que se muestra en el video.
+       *
+       * No se si es la forma correcta de hacerlo, o si había que hacerlo
+       * haciendo uso de $_COOKIE, pero si es así, la verdad que me vendría
+       * bien saber como hacerlo, ya que no he encontrado esa información en ningún
+       * video de explicación ni los apuntes.
+       */
+
+      if (isset($lista_actualizada)) {
+
+          print "<h3>Páginas Visitadas</h3>";
+          foreach ($lista_actualizada as $elemento => $seccion) {
+              $link = urlencode($seccion['link']);
+              $nombre = $seccion['nombre'];
+              $fecha = $seccion['hora'];
+
+              print "<a href='?ver=$link'>$nombre ($fecha)</a>";
+          }
+      }
     ?>
     <BR>
     <BR>
--- DWES/php-projects/dwes03/cookies/index.php
+++ DWES/php-projects/dwes03/cookies/index.php
@@ -3,24 +3,84 @@
 
   $ver = $_GET['ver'] ?? null;
 
-//Si no se ha especificado $_GET['ver'] hemos que $ver tenga el "link" de la sección por defecto
+  //Si no se ha especificado $_GET['ver'] hemos que $ver tenga el "link" de la sección por defecto
+
   if (is_null($ver) || empty($ver)) {
       //Buscamos la posición del array $secciones donde está el nombre de sección por defecto
       $posSeccionDefecto = array_search(SECCION_DEFECTO, array_column($secciones, 'nombre'));
       if ($posSeccionDefecto !== false)
           $ver = $secciones[$posSeccionDefecto]['link'];
   }
-//$seccion = sección a mostrar
+
+  //$seccion = sección a mostrar
+
   if (is_string($ver) && ($pos = array_search($ver, array_column($secciones, 'link'))) !== false) {
       $seccion = $secciones[$pos];
-  }
+      /*
+       * Comprobamos que existen las cookies, en caso de que existan y no se hayan modificado, creamos
+       * un nuevo array con la lista de sitios vistados y lo poblamos con las secciones visitadas.
+       */
 
-//TODO: 
-// 1.- obtener las cookies y verificar que no ha sido modificada (calcular hash)
-// 2.- deserializar la cookie con la listad e sitios visitados.
-// 3.- añadir la seccion recien visitada sin duplicados (usa la función time() para el momento de acceso), insertando el primero en la cookie
-// 4.- envia la cookie y el hash
-// 5.- en footer.php, muestra la lista de sitios visitados y la fecha en orden cronológico.
+      if (isset($_COOKIE['lista_sitios_visitados']) && isset($_COOKIE['hash_lista_sitios_visitados'])) {
+          if (hash('sha256', $_COOKIE['lista_sitios_visitados']) == $_COOKIE['hash_lista_sitios_visitados']) {
+
+              $lista_visitados = unserialize($_COOKIE['lista_sitios_visitados']);
+
+              if (is_array($lista_visitados)) {
+                  $nombre_ultima = $seccion['nombre'];
+                  $enlace_ultima = $seccion['link'];
+
+                  $lista_actualizada = [];
+                  $lista_actualizada[] = [
+                      "nombre" => $nombre_ultima,
+                      "link" => $enlace_ultima,
+                      "hora" => date("d/m/Y H:i", time())
+                  ];
+
+                  foreach ($lista_visitados as $elemento => $registro)
+                      if ($registro['nombre'] != $nombre_ultima) {
+                          $lista_actualizada[] = [
+                              "nombre" => $registro['nombre'],
+                              "link" => $registro['link'],
+                              "hora" => $registro['hora']
+                          ];
+                      }
+
+                  if (count($lista_actualizada) > 5)
+                      array_pop($lista_actualizada);
+
+                  setcookie("lista_sitios_visitados", serialize($lista_actualizada));
+                  setcookie("hash_lista_sitios_visitados", hash('sha256', serialize($lista_actualizada)));
+              }
+          } else {
+              $nombre_ultima = $seccion['nombre'];
+              $enlace_ultima = $seccion['link'];
+
+              $lista_actualizada = [];
+              $lista_actualizada[] = [
+                  "nombre" => $nombre_ultima,
+                  "link" => $enlace_ultima,
+                  "hora" => date("d/m/Y H:i", time())
+              ];
+
+              setcookie("lista_sitios_visitados", serialize($lista_actualizada));
+              setcookie("hash_lista_sitios_visitados", hash('sha256', serialize($lista_actualizada)));
+          }
+      } else {
+          $nombre_ultima = $seccion['nombre'];
+          $enlace_ultima = $seccion['link'];
+
+          $lista_actualizada = [];
+          $lista_actualizada[] = [
+              "nombre" => $nombre_ultima,
+              "link" => $enlace_ultima,
+              "hora" => date("d/m/Y H:i", time())
+          ];
+
+          setcookie("lista_sitios_visitados", serialize($lista_actualizada));
+          setcookie("hash_lista_sitios_visitados", hash('sha256', serialize($lista_actualizada)));
+      }
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,7 +95,6 @@
         <div>
 
             <?php
-//Buscamos el texto de "link" almacenado en $ver en las secciones y extraemos el archivo o contenido
               if (isset($seccion)) {
                   if (isset($seccion['contenido'])) {
                       $longitud = strlen($seccion['contenido']);
