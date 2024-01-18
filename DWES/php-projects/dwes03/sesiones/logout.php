d77051dbc568319b2efc3aea15341506585c9ba1
Fco Sueza <fcosueza@disroot.org>
2024-01-18 12:55:13 +0100

    Added logout script to exercise 3.4 in DWES

--- DWES/php-projects/dwes03/sesiones/logout.php
+++ DWES/php-projects/dwes03/sesiones/logout.php
@@ -1,4 +1,11 @@
 <?php
 
-/*TODO: Borra datos del usuario autenticado de la sesion, del sello de tiempo para control de inactividad y despide al usuario*/
-/*IMPORTANTE: ¡cuidado! Si recargo esta página después de hacer logout no debe mostrar error php */
+  // Comprobamos si hay una sessión iniciada y la destruimos en caso afirmativo.
+  if (isset($_SESSION['id'])) {
+      session_unset();
+      session_destroy();
+  }
+
+  // Redirigimos a la página login.
+  header('Location: login.php');
+  exit();
