77144cb34f2df1e240f2aaeeee0d0d3b4e2fa023
Fco Sueza <fcosueza@disroot.org>
2024-03-01 08:41:27 +0100

    UwU

--- DWES/php-projects/DWES_proyecto_alumnado_FEB_2022-2023/alumnado_nb/ej1/funciones.php
+++ DWES/php-projects/DWES_proyecto_alumnado_FEB_2022-2023/alumnado_nb/ej1/funciones.php
@@ -1,57 +1,55 @@
 <?php
 
-  require_once('./vars.php');
+  // Vamos a crear una variable global con los candidatos elegidos
 
   /**
    * Función que, a partir de la lista de participantes, ubicada en la variable de ámbito global
    * $participantes, genera un array de $n candidatos finalistas de manera aleatoria
    *
-   * @param $n: Número de candidatos que serán seleccionados
+   * @param $num Número de candidatos que serán seleccionados
    *
    * @return: Array con los $n candidatos seleccionados
    */
   function getCandidatos($num) {
-      $randomHistory = [];
-      $heroes = [];
+      global $participantes;
 
-      for ($i = 0; $i < 4; $i++) {
-          $random = rand(1, 8);
+      $indexes = array_rand($participantes, $num);
 
-          if (!in_array($i, $randomHistory)) {
-              $heroes[] = $participantes[$i];
-          }
+      foreach ($indexes as $index) {
+          $candidatos[] = $participantes[$index];
       }
 
-      return $participantes;
+      return $candidatos;
   }
 
   /**
-   * Función que, a partir de la lista de candidatos seleccionados, ubicada en la variable pasada como parámetro $candidatos, genera una cadena HTML con el formulario para poder elegir el ganador final
+   * Función que, a partir de la lista de candidatos seleccionados, ubicada en la variable pasada como parámetro $candidatos,
+   * genera una cadena HTML con el formulario para poder elegir el ganador final
    *
-   * @param $candidatos: Un array con los seleccionados a ganador.
+   * @param $candidatos Un array con los seleccionados a ganador.
    *
    * @return: Cadena con el formulario que se imprimirá en el html con los candidatos seleccionados.
    */
   function getFormularioCandidatosMarkup($candidatos) {
       //Debes modificar esta función para que el formulario se construya dinámicament con los datos de $candidatos
-      $output = '';
       $output = '<form action="ganador.php" method="post">
     <div class="candidatoContainer">
-      <h2>Clark Kent</h2>
-      <img src="./img/Henry_Cavill_by_Gage_Skidmore_2.jpg"><br><input type="submit" value="Seleccionar" name="seleccionar[1]">
+      <h2>' . $candidatos[0]["nombre"] . '</h2>
+      <img src="' . $candidatos[0]["imagen_url"] . '"><br><input type="submit" value="Seleccionar" name="seleccionar[ ' . $candidatos[0]["nombre"] . ']">
     </div>
     <div class="candidatoContainer">
-      <h2>Bruce Wayne</h2>
-      <img src="./img/Christianbale.jpg"><br><input type="submit" value="Seleccionar" name="seleccionar[2]">
+      <h2>' . $candidatos[1]["nombre"] . '</h2>
+      <img src="' . $candidatos[1]["imagen_url"] . '"><br><input type="submit" value="Seleccionar" name="seleccionar[' . $candidatos[1]["nombre"] . ']">
     </div>
     <div class="candidatoContainer">
-      <h2>Diana Prince</h2>
-      <img src="./img/Diana_in_White.png"><br><input type="submit" value="Seleccionar" name="seleccionar[3]">
+      <h2>' . $candidatos[2]["nombre"] . '</h2>
+      <img src="' . $candidatos[2]["imagen_url"] . '"><br><input type="submit" value="Seleccionar" name="seleccionar[' . $candidatos[2]["nombre"] . ']">
     </div>
     <div class="candidatoContainer">
-      <h2>Barry Allen</h2>
-      <img src="./img/Barry-Hallen.png"><br><input type="submit" value="Seleccionar" name="seleccionar[4]">
+      <h2>' . $candidatos[3]["nombre"] . '</h2>
+      <img src="' . $candidatos[3]["imagen_url"] . '"><br><input type="submit" value="Seleccionar" name="seleccionar[' . $candidatos[3]["nombre"] . ']">
     </div>
   </form>';
+
       return $output;
   }
