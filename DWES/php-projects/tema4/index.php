fff8cc9d7eb5bbac81cc0d3b931bd82772fd7b3a
Fco Sueza <fcosueza@disroot.org>
2024-03-13 08:44:10 +0100

    Added netbeans tema4 project to php-project for practicing tema4's exercises

---
+++ DWES/php-projects/tema4/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Fco Sueza" />
+        <meta name="description" content="Web para desarrollar las actividades en PHP del tema 4 de DWEC" />
+
+        <title>DWEC - Tema 4</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Desarrollo Web en el Entorno Servidor: Tema 2</h1>
+            <p>En esta web vamos a ver todos los ejercicios y tareas relacionados con el Tema 1 de DWEC</p>
+        </header>
+        <main>
+            <h3>Ejercicios y Tarea:</h3>
+            <ul>
+
+        </main>
+    </body>
+</html>
