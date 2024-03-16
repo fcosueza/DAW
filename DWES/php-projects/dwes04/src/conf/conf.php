0cb07b273c3f590b9eddb39b2984ff0fafdbc858
Fco Sueza <fcosueza@disroot.org>
2024-03-16 09:30:22 +0100

    Added conf.php file with constans definitions

---
+++ DWES/php-projects/dwes04/src/conf/conf.php
@@ -0,0 +1,21 @@
+<?php
+
+  /*
+   * Archivo donde se definen todas la variables globales de la aplicación,
+   * como los datos de la base de datos o los directorios de Smarty,
+   *
+   */
+
+
+  // Constantes de la base de datos
+
+  define("DB_DNS", "mysql=host:localhost;dbname=respira");
+  define("DB_USER", "root");
+  define("DB_PASS", "H0l0caust0");
+
+  // Constantes para Smarty
+
+  define("TEMPLATE_DIR", "/templates");
+  define("TEMPLATE_COMPILE_DIR", "/templates_build");
+  define("CACHE_DIR", "/cache");
+  
