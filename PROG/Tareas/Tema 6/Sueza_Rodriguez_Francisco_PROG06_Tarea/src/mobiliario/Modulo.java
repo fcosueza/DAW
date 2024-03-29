88ac53eaf05ec923509bd00f1e2529050b776abe
Fco Sueza <fcosueza@disroot.org>
2024-03-29 11:03:44 +0100

    Added base project for exercise 6 in PROG

---
+++ PROG/Tareas/Tema 6/Sueza_Rodriguez_Francisco_PROG06_Tarea/src/mobiliario/Modulo.java
@@ -0,0 +1,19 @@
+package mobiliario;
+
+/**
+ * Clase que representa un <code>enum</code> con los módulos que se pueden
+ * añadir a un mueble personalizable.
+ *
+ * @author profe
+ * @author Francisco Sueza Rodríguez
+ */
+public enum Modulo {
+    BALDA,
+    CAJON,
+    CON_PUERTA,
+    CESTO,
+    ZAPATERO,
+    BANDEJA,
+    PANTALONERO,
+    CON_BARRA
+}
