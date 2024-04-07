4ea47484b0165dda47e947a77b056c3f9933740a
Fco Sueza <fcosueza@disroot.org>
2024-04-07 12:42:27 +0200

    Added migrations to exercise 5 in DWES

--- DWES/php-projects/dwes05/database/migrations/2024_04_07_101559_ubicaciones.php
+++ DWES/php-projects/dwes05/database/migrations/2024_04_07_101559_ubicaciones.php
@@ -1,24 +1,28 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+  use Illuminate\Database\Migrations\Migration;
+  use Illuminate\Database\Schema\Blueprint;
+  use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        //
-    }
+return new class extends Migration {
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        //
-    }
-};
+      /**
+       * Run the migrations.
+       */
+      public function up(): void {
+          Schema::create('ubicaciones', function (Blueprint $table) {
+              $table->id();
+              $table->timestamps();
+              $table->string('nombre', 50);
+              $table->text("descripcion");
+              $table->set('dias', ['L', 'M', 'X', 'J', 'V', 'S', 'D']);
+          });
+      }
+
+      /**
+       * Reverse the migrations.
+       */
+      public function down(): void {
+          Schema::dropIfExists('ubicaciones');
+      }
+  };
--- DWES/php-projects/dwes05/database/migrations/2024_04_07_101612_talleres.php
+++ DWES/php-projects/dwes05/database/migrations/2024_04_07_101612_talleres.php
@@ -1,24 +1,33 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+  use Illuminate\Database\Migrations\Migration;
+  use Illuminate\Database\Schema\Blueprint;
+  use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        //
-    }
+return new class extends Migration {
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        //
-    }
-};
+      /**
+       * Run the migrations.
+       */
+      public function up(): void {
+          Schema::create('talleres', function (Blueprint $table) {
+              $table->id();
+              $table->unsignedBigInteger('ubicacion_id');
+              $table->foreign('ubicacion_id')->references('id')->on('ubicaciones');
+              $table->timestamps();
+              $table->string('nombre', 50);
+              $table->text('descripcion');
+              $table->set('dia_semana', ['L', 'M', 'X', 'J', 'V', 'S', 'D']);
+              $table->time('hora_inicio');
+              $table->time('hora_fin');
+              $table->unsignedInteger('cupo_maximo');
+          });
+      }
+
+      /**
+       * Reverse the migrations.
+       */
+      public function down(): void {
+          Schema::dropIfExists('talleres');
+      }
+  };
