8f764b8eee57b7c6e9e012caa31eee55e28b03a7
Fco Sueza <fcosueza@disroot.org>
2024-05-19 12:50:41 +0200

    Seed database for exercise 10 in PROG

--- PROG/Tareas/Tema 10/Sueza_Rodriguez_Francisco_PROG_Tarea10/src/resources/cargaBD.sql
+++ PROG/Tareas/Tema 10/Sueza_Rodriguez_Francisco_PROG_Tarea10/src/resources/cargaBD.sql
@@ -1,30 +1,31 @@
 
 -- Datos para tabla Peliculas
--- 111,'El señor de los anillos: La comunidad del anillo','Frodo recibe el anillo de poder', '2001-12-19'
--- 112,'El señor de los anillos: Las dos torres','Frodo pasa entre dos torres', '2002-12-18');
--- 113,'El señor de los anillos: El retorno del rey','Frodo tira el anillo', '2003-12-17');
--- 114,'Matrix','Neo descubre la verdad', '2003-12-04');
--- 115,'Matrix Reloaded','Neo luchas contra las máquinas', '2005-12-05');
--- 116,'Matrix Revollution','Neo acaba con las máquinas', '2006-12-04');
--- 117,'Avenger civil war','Thanos va por las gemas y hace la purga', '2006-12-04');
--- 118,'Avenger end game','Vuelven al pasado tras el multiverso para cambiar el presente', '2006-12-04');
 
+INSERT INTO Peliculas VALUES(111,'El señor de los anillos: La comunidad del anillo','Frodo recibe el anillo de poder', '2001-12-19');
+INSERT INTO Peliculas VALUES(112,'El señor de los anillos: Las dos torres','Frodo pasa entre dos torres', '2002-12-18');
+INSERT INTO Peliculas VALUES(113,'El señor de los anillos: El retorno del rey','Frodo tira el anillo', '2003-12-17');
+INSERT INTO Peliculas VALUES(114,'Matrix','Neo descubre la verdad', '2003-12-04');
+INSERT INTO Peliculas VALUES(115,'Matrix Reloaded','Neo luchas contra las máquinas', '2005-12-05');
+INSERT INTO Peliculas VALUES(116,'Matrix Revolutions','Neo acaba con las máquinas', '2006-12-04');
+INSERT INTO Peliculas VALUES(117,'Avenger Infinity War','Thanos va por las gemas y hace la purga', '2006-12-04');
+INSERT INTO Peliculas VALUES(118,'Avenger end game','Vuelven al pasado tras el multiverso para cambiar el presente', '2006-12-04');
 
 -- Datos para tabla Plataformas
---'333','Netflix','https://images-na.ssl-images-amazon.com/images/I/51LGj5--KsL.png'
---'334','Movistar','https://www.tuexperto.com/wp-content/uploads/2018/09/80dd8ee71e2be472cf86ee124476966a.png'
---'335','Disney+','https://store-images.s-microsoft.com/image/apps.14187.14495311847124170.7646206e-bd82-4cf0-8b8c-d06a67bc302c.2e474878-acb7-4afb-a503-c2a1a32feaa8'
---'336','HBO max','https://yt3.googleusercontent.com/bjkwQJm7D-ynsH_4ySvk-sqM802R1RYuYDqTazAF-g3gupsidu981t-uA3FFzlkdkK5txkXsLjg=s900-c-k-c0x00ffffff-no-rj'
 
--- Datos para tabla Disponible_en 
--- 333, 111, '2018-01-12'
--- 333, 112, '2018-02-13'
--- 333, 115, '2018-03-14'
--- 333, 116, '2018-04-15'
--- 334, 112, '2020-05-02'
--- 334, 113, '2020-06-02'
--- 334, 114, '2020-07-02'
--- 334, 115, '2021-08-02'
--- 334, 116, '2021-09-02'
--- 335, 117, '2022-01-10'
--- 335, 118, '2022-05-02'
+INSERT INTO Plataformas VALUES(333,'Netflix','https://images-na.ssl-images-amazon.com/images/I/51LGj5--KsL.png');
+INSERT INTO Plataformas VALUES(334,'Movistar','https://www.tuexperto.com/wp-content/uploads/2018/09/80dd8ee71e2be472cf86ee124476966a.png');
+INSERT INTO Plataformas VALUES(335,'Disney+','https://store-images.s-microsoft.com/image/apps.14187.14495311847124170.7646206e-bd82-4cf0-8b8c-d06a67bc302c.2e474878-acb7-4afb-a503-c2a1a32feaa8');
+INSERT INTO Plataformas VALUES(336,'HBO Max','https://yt3.googleusercontent.com/bjkwQJm7D-ynsH_4ySvk-sqM802R1RYuYDqTazAF-g3gupsidu981t-uA3FFzlkdkK5txkXsLjg=s900-c-k-c0x00ffffff-no-rj');
+
+-- Datos para tabla Disponible_en
+
+INSERT INTO Disponible_en VALUES(333, 111, '2018-01-12');
+INSERT INTO Disponible_en VALUES(333, 112, '2018-02-13');
+INSERT INTO Disponible_en VALUES(333, 116, '2018-04-15');
+INSERT INTO Disponible_en VALUES(334, 112, '2020-05-02');
+INSERT INTO Disponible_en VALUES(334, 113, '2020-06-02');
+INSERT INTO Disponible_en VALUES(334, 114, '2020-07-02');
+INSERT INTO Disponible_en VALUES(334, 115, '2021-08-02');
+INSERT INTO Disponible_en VALUES(334, 116, '2021-09-02');
+INSERT INTO Disponible_en VALUES(335, 117, '2022-01-10');
+INSERT INTO Disponible_en VALUES(335, 118, '2022-05-02');
--- PROG/Tareas/Tema 10/Sueza_Rodriguez_Francisco_PROG_Tarea10/src/resources/creaBD.sql
+++ PROG/Tareas/Tema 10/Sueza_Rodriguez_Francisco_PROG_Tarea10/src/resources/creaBD.sql
@@ -7,21 +7,21 @@ DROP TABLE IF EXISTS Peliculas ;
 --
 
 CREATE TABLE IF NOT EXISTS Peliculas(
-    codigo int NOT NULL PRIMARY KEY auto_increment,
-    titulo varchar(30) NOT NULL,
+    codigo int NOT NULL PRIMARY KEY,
+    titulo varchar(100) NOT NULL,
     sinopsis varchar(200) NOT NULL,
     fEstreno DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Plataformas (
-    codigo int NOT NULL PRIMARY KEY auto_increment,
-    nombre varchar(20) NOT NULL,
-    urlLogotipo varchar(100) NOT NULL
+    codigo int NOT NULL PRIMARY KEY,
+    nombre varchar(50) NOT NULL,
+    urlLogotipo varchar(400) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Disponible_en (
+    codPlataforma int NOT NULL,
     codPelicula int NOT NULL,
-    codPlataforma INT NOT NULL,
     fDisponibilidad DATE NOT NULL,
     FOREIGN KEY(codPelicula) REFERENCES PUBLIC.Peliculas(codigo) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY(codPlataforma) REFERENCES PUBLIC.Plataformas(codigo) ON UPDATE CASCADE ON DELETE CASCADE,
