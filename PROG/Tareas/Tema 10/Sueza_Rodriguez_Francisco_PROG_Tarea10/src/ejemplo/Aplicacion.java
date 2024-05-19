afb178057693d5f6b2dcf7490c741251e63d7be9
Fco Sueza <fcosueza@disroot.org>
2024-05-19 15:01:04 +0200

    Added methods to query DB and show results for exercise 10 in PROG

--- PROG/Tareas/Tema 10/Sueza_Rodriguez_Francisco_PROG_Tarea10/src/ejemplo/Aplicacion.java
+++ PROG/Tareas/Tema 10/Sueza_Rodriguez_Francisco_PROG_Tarea10/src/ejemplo/Aplicacion.java
@@ -14,6 +14,7 @@ import org.h2.tools.Server;
 import static java.lang.System.*;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
 import utilidades.ES;
@@ -81,7 +82,7 @@ public class Aplicacion {
             //Conectamos con la base de datos.
             //El try-with-resources asegura que se cerrará la conexión al salir.
             String[] wsArgs = {"-baseDir", System.getProperty("user.dir"), "-browser"};
-            try ( Connection con = DriverManager.getConnection(URL_CONEXION + PARAMS, "", "")) {
+            try (Connection con = DriverManager.getConnection(URL_CONEXION + PARAMS, "", "")) {
 
                 // Iniciamos el servidor web interno (consola H2 para depuraciones)
                 Server sr = Server.createWebServer(wsArgs);
@@ -139,7 +140,7 @@ public class Aplicacion {
                             case 3:
                                 modificarPlataforma(con);
                                 break;
-                            case 4:                                
+                            case 4:
                                 mostrarPeliculasPorPlataformas(con);
                                 break;
                         }
@@ -176,7 +177,7 @@ public class Aplicacion {
     public static boolean crearTablas(Connection con) {
         boolean todoBien = false;
 
-        try ( Statement st = con.createStatement()) {
+        try (Statement st = con.createStatement()) {
 
             String sqlScript = loadResourceAsString(ESTRUCTURA_DB);
             if (sqlScript != null) {
@@ -205,7 +206,7 @@ public class Aplicacion {
     private static boolean insertarDatosTablas(Connection con) {
         boolean todoBien = false;
 
-        try ( Statement st = con.createStatement()) {
+        try (Statement st = con.createStatement()) {
 
             String sqlScript = loadResourceAsString(INSERTA_DB);
             if (sqlScript != null) {
@@ -232,7 +233,7 @@ public class Aplicacion {
         String resource = null;
         InputStream is = Aplicacion.class.getResourceAsStream(resourceName);
         if (is != null) {
-            try ( InputStreamReader isr = new InputStreamReader(is);  BufferedReader br = new BufferedReader(isr);) {
+            try (InputStreamReader isr = new InputStreamReader(is); BufferedReader br = new BufferedReader(isr);) {
                 resource = br.lines().collect(Collectors.joining("\n"));
             } catch (IOException ex) {
                 System.out.printf("Problema leyendo el recurso como cadena: %S\n ", resourceName);
@@ -247,7 +248,34 @@ public class Aplicacion {
      * @param con Conexión a la BD
      */
     private static void mostrarPeliculas(Connection con) {
-        //Ejecutamos la consulta sobre la tabla peliculas y mostramos su contenido.
+
+        // Comprobamos que la conexión no sea null
+        if (con != null) {
+
+            // Creamos un try-resource-catch con la consulta y mostramos los resultados
+            try (Statement consulta = con.createStatement()) {
+                ResultSet resultado = consulta.executeQuery("SELECT * FROM peliculas");
+
+                System.out.printf("\n\n------------ Películas Disponibles --------------\n");
+                System.out.printf("| Código - Título - Sinopsis - Fecha de estreno |\n");
+                System.out.printf("-------------------------------------------------\n\n");
+
+                while (resultado.next()) {
+                    int codigo = resultado.getInt("codigo");
+                    String titulo = resultado.getString("titulo");
+                    String sinopsis = resultado.getString("sinopsis");
+                    Date fecha = resultado.getDate("fEstreno");
+
+                    System.out.printf("%d - %s - %s - %s \n", codigo, titulo, sinopsis, fecha);
+                }
+
+            } catch (SQLException e) {
+                System.err.printf("Se ha producido un error al ejecutar la consulta SQL.");
+            }
+        } else {
+            System.err.printf("La conexión a la base de datos no puede tener el valor null.");
+
+        }
     }
 
     /**
@@ -256,12 +284,64 @@ public class Aplicacion {
      * @param con Conexión a la BD
      */
     private static void modificarPlataforma(Connection con) {
-        //Mostramos las plataformas disponibles.
-        
-        //Pediremos el código de la plataforma y el nuevo nombre.
-        
-        // Ejecutamos la sentencia SQL de actualización
-        
+        int codigoPlataforma;
+        String nuevoNombre;
+
+        // Comprobamos que la conexión no sea null
+        if (con != null) {
+
+            // Creamos un try-resource-catch con la consulta para mostrar todas las plataformas
+            try (Statement consulta = con.createStatement()) {
+                ResultSet resultado = consulta.executeQuery("SELECT * FROM plataformas");
+
+                System.out.printf("\n\n------------ Plataformas Disponibles -----------\n");
+                System.out.printf("| Código - Nombre                              |\n");
+                System.out.printf("------------------------------------------------\n\n");
+
+                while (resultado.next()) {
+                    int codigo = resultado.getInt("codigo");
+                    String nombre = resultado.getString("nombre");
+
+                    System.out.printf("%d - %s\n", codigo, nombre);
+                }
+
+            } catch (SQLException e) {
+                System.err.printf("Se ha producido un error al ejecutar la consulta SQL.");
+            }
+
+            // Pedímos el codigo de la plataforma a modificar y su nuevo nombre
+            System.out.printf("\n\nIntroduzca el código de la plataforma a modificar: ");
+            codigoPlataforma = ES.leeEntero();
+
+            System.out.printf("\n\nIntroduzca el nuevo nombre de la plataforma: ");
+            nuevoNombre = ES.leeCadena();
+
+            // Creamos la consulta SQL con paramétros
+            String consulta = "UPDATE plataformas SET nombre=? WHERE codigo=?";
+
+            // Creamos otro try-resource-catch para realizar la actualización del nombre de la plataforma
+            try (PreparedStatement consultaPreparada = con.prepareStatement(consulta)) {
+                // Enlazamos los paramétros con la consulta
+                consultaPreparada.setString(1, nuevoNombre);
+                consultaPreparada.setInt(2, codigoPlataforma);
+
+                // Ejecutamos la consulta y almacenamos el número de registros afectados
+                int registrosActualizados = consultaPreparada.executeUpdate();
+
+                // Mostramos información sobre la ejecuciónd de la consulta
+                if (registrosActualizados > 0) {
+                    System.out.printf("La actualización se ha realizado correctamente.");
+                } else {
+                    System.out.printf("No se ha encontrado ninguna plataforma con ese código (Actualización fallida)");
+                }
+
+            } catch (SQLException e) {
+                System.err.printf("Se ha producido un error al ejecutar la consulta SQL.");
+
+            }
+        } else {
+            System.err.printf("La conexión a la base de datos no puede tener el valor null.");
+        }
     }
 
     /**
@@ -270,12 +350,59 @@ public class Aplicacion {
      * @param con Conexión a la BD
      */
     private static void borrarPlataforma(Connection con) {
-        //Mostramos las plataformas disponibles.
-        
-        //pedimos el código de la plataforma a borrar
-       
-        // Ejecutamos la sentencia SQL de borrado
-        
+        int codigoPlataforma;
+
+        // Comprobamos que la conexión no sea null
+        if (con != null) {
+
+            // Creamos un try-resource-catch con la consulta para mostrar todas las plataformas
+            try (Statement consulta = con.createStatement()) {
+                ResultSet resultado = consulta.executeQuery("SELECT * FROM plataformas");
+
+                System.out.printf("\n\n------------ Plataformas Disponibles -----------\n");
+                System.out.printf("| Código - Nombre                              |\n");
+                System.out.printf("------------------------------------------------\n\n");
+
+                while (resultado.next()) {
+                    int codigo = resultado.getInt("codigo");
+                    String nombre = resultado.getString("nombre");
+
+                    System.out.printf("%d - %s\n", codigo, nombre);
+                }
+
+            } catch (SQLException e) {
+                System.err.printf("Se ha producido un error al ejecutar la consulta SQL.");
+            }
+
+            // Pedímos el codigo de la plataforma a borrar
+            System.out.printf("\n\nIntroduzca el código de la plataforma que desea borrar: ");
+            codigoPlataforma = ES.leeEntero();
+
+            // Creamos la consulta SQL con paramétros
+            String consulta = "DELETE FROM plataformas WHERE codigo=?";
+
+            // Creamos otro try-resource-catch para realizar la actualización del nombre de la plataforma
+            try (PreparedStatement consultaPreparada = con.prepareStatement(consulta)) {
+                // Enlazamos los paramétros con la consulta
+                consultaPreparada.setInt(1, codigoPlataforma);
+
+                // Ejecutamos la consulta y almacenamos el número de registros afectados
+                int registrosActualizados = consultaPreparada.executeUpdate();
+
+                // Mostramos información sobre la ejecuciónd de la consulta
+                if (registrosActualizados > 0) {
+                    System.out.printf("La plataforma se ha borrado con éxito.");
+                } else {
+                    System.out.printf("No se ha encontrado ninguna plataforma con ese código (Eliminación fallida)");
+                }
+
+            } catch (SQLException e) {
+                System.err.printf("Se ha producido un error al ejecutar la consulta SQL.");
+
+            }
+        } else {
+            System.err.printf("La conexión a la base de datos no puede tener el valor null.");
+        }
     }
 
     /**
@@ -284,9 +411,63 @@ public class Aplicacion {
      * @param con Conexión a la BD
      */
     private static void mostrarPeliculasPorPlataformas(Connection con) {
-        /*Para cada plataforma existente en la tabla Plataformas, debes mostrar el nombre de la 
-          plataforma en la consola y buscar las películas asociadas a la plataforma en la tabla Disponible_en. 
-          Para cada película, usando el código de la película buscar el nombre para mostrarlo en la consola. */
-       
+        /*
+         * Para llevar a cabo esta consulta, primero vamos a realizar una consulta que muestre
+         * todas las plataformas, iterando por el resultado con el método .next() para a continuación
+         * realizar un INNER JOIN entre la tabla Películas y Disponible_en, mostrando todas las películas
+         * pertenecientes a esa plataforma.
+         */
+
+        if (con != null) {
+
+            // Mostramos la cabecera del resultado
+            System.out.printf("\n\n--------------------------------------------\n");
+            System.out.printf("|            Películas por Plataforma          |\n");
+            System.out.printf("------------------------------------------------\n\n");
+
+            // Usamos un try-resources para realizar la consulta sobre las plataformas
+            try (Statement consultaPlataformas = con.createStatement()) {
+                ResultSet resultadoPlataformas = consultaPlataformas.executeQuery("SELECT codigo, nombre FROM Plataformas");
+
+                // Iteramos por las plataformas
+                while (resultadoPlataformas.next()) {
+                    int codPlataforma = resultadoPlataformas.getInt("codigo");
+                    String nombrePlataforma = resultadoPlataformas.getString("nombre");
+
+                    // Mostramos el nombre de la plataforma
+                    System.out.printf("\n\nPlataforma: %s\n", nombrePlataforma);
+                    System.out.println("-----------------------------------------------");
+
+                    // Creamos la consulta para las películas, empleando un INNER JOIN
+                    String peliculasQuery = "SELECT peliculas.titulo "
+                            + "FROM peliculas INNER JOIN disponible_en "
+                            + "ON (peliculas.codigo = disponible_en.codpelicula) AND (disponible_en.codPlataforma=?)";
+
+                    // Creamos el try-resource para realizar la consulta sobre lás películas
+                    try (PreparedStatement consultaPeliculas = con.prepareStatement(peliculasQuery)) {
+                        // Enlazamos el parametro
+                        consultaPeliculas.setInt(1, codPlataforma);
+
+                        if (consultaPeliculas.execute()) {
+                            ResultSet resultadoPeliculas = consultaPeliculas.getResultSet();
+
+                            // Iteramos el resultado y mostramos las peliculas
+                            while (resultadoPeliculas.next()) {
+                                String nombrePelicula = resultadoPeliculas.getString("titulo");
+
+                                System.out.printf("%s\n", nombrePelicula);
+                            }
+
+                            System.out.println("-----------------------------------------------");
+                        }
+                    } catch (SQLException e) {
+                        System.err.printf("Se ha producido un error al ejecutar la consulta SQL.");
+                    }
+                }
+            } catch (SQLException e) {
+                System.err.printf("Se ha producido un error al ejecutar la consulta SQL.");
+            }
+        }
+
     }
 }
