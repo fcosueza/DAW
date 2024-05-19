e5caf4c16a2239d4b37454a895d5c79e328c12b0
Fco Sueza <fcosueza@disroot.org>
2024-05-19 11:16:44 +0200

    SQL script for DB creation in exercise 10 on PROG

--- PROG/Tareas/Tema 10/Sueza_Rodriguez_Francisco_PROG_Tarea10/src/resources/creaBD.sql
+++ PROG/Tareas/Tema 10/Sueza_Rodriguez_Francisco_PROG_Tarea10/src/resources/creaBD.sql
@@ -5,3 +5,26 @@ DROP TABLE IF EXISTS Peliculas ;
 --
 -- Estructura de las tablas
 --
+
+CREATE TABLE IF NOT EXISTS Peliculas(
+    codigo int NOT NULL PRIMARY KEY auto_increment,
+    titulo varchar(30) NOT NULL,
+    sinopsis varchar(200) NOT NULL,
+    fEstreno DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Plataformas (
+    codigo int NOT NULL PRIMARY KEY auto_increment,
+    nombre varchar(20) NOT NULL,
+    urlLogotipo varchar(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Disponible_en (
+    codPelicula int NOT NULL,
+    codPlataforma INT NOT NULL,
+    fDisponibilidad DATE NOT NULL,
+    FOREIGN KEY(codPelicula) REFERENCES PUBLIC.Peliculas(codigo) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY(codPlataforma) REFERENCES PUBLIC.Plataformas(codigo) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT PK_Disponible PRIMARY KEY (codPelicula, codPlataforma, fDisponibilidad)
+
+)
