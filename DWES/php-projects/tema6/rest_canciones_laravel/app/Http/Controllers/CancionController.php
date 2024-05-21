0d77fef9321a0fe8fbcf7ced7ccd97f9898442e0
Fco Sueza <fcosueza@disroot.org>
2024-05-21 09:55:39 +0200

    Add migration, model and controller in Laravel project

---
+++ DWES/php-projects/tema6/rest_canciones_laravel/app/Http/Controllers/CancionController.php
@@ -0,0 +1,66 @@
+<?php
+
+  namespace App\Http\Controllers;
+
+  use Illuminate\Http\Request;
+  use App\Models\Cancion;
+
+  class CancionController extends Controller {
+
+      /**
+       * Display a listing of all songs
+       */
+      public function index() {
+          return Cancion::all();
+      }
+
+      /**
+       * Show the form for creating a new resource.
+       */
+      public function create() {
+
+      }
+
+      /**
+       * Store a new song
+       */
+      public function store(Request $request) {
+          $cancion = Cancion::create($request->all());
+
+          return response()->json($cancion, 201);
+      }
+
+      /**
+       * Display the specified song
+       */
+      public function show(string $id) {
+          return Cancion::find();
+      }
+
+      /**
+       * Show the form for editing the specified resource.
+       */
+      public function edit(string $id) {
+          //
+      }
+
+      /**
+       * Update the specified resource in storage.
+       */
+      public function update(Request $request, string $id) {
+          $cancion = Cancion::findOrFail($id);
+          $cancion->update($request->all());
+
+          return response->json($cancion, 200);
+      }
+
+      /**
+       * Remove the specified resource from storage.
+       */
+      public function destroy(string $id) {
+          $cancion = Cancion::findOrFail($id);
+          $cancion->delete();
+
+          return response->json($cancion, 200);
+      }
+  }
---
+++ DWES/php-projects/tema6/rest_canciones_laravel/app/Models/Cancion.php
@@ -0,0 +1,14 @@
+<?php
+
+  namespace App\Models;
+
+  use Illuminate\Database\Eloquent\Factories\HasFactory;
+  use Illuminate\Database\Eloquent\Model;
+
+  class Cancion extends Model {
+
+      use HasFactory;
+
+      protected $table = "canciones";
+      protected $fillable = ["titulo", "artista", "genero"];
+  }
